011000970bf3ef0550995e10a3087e1a32e92793
Kateryna Vasiuk <catherine.vasyuk11@gmail.com>
2023-03-03 15:40:03 +0200

    sum

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,41 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryRef = document.querySelector(".gallery");
+/* --------------------------------- Галерея -------------------------------- */
+const galleryHTML = galleryItems
+  .map(({ original, description, preview }) => {
+    return `
+    <div class="gallery__item">
+    <a class="gallery__link" href="${original} ">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+      />
+    </a>
+  </div>
+  `;
+  })
+  .join("");
+galleryRef.insertAdjacentHTML("beforeend", galleryHTML);
 
-console.log(galleryItems);
+galleryRef.addEventListener("click", onClickGallery);
+/* -------------------------------------------------------------------------- */
+function onClickGallery(e) {
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+  e.preventDefault();
+  const BigImageLink = e.target.dataset.source;
+  const instance = basicLightbox.create(` <img src="${BigImageLink}">`);
+  instance.show();
+  /* ----------------------------- close when esc ----------------------------- */
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") {
+      instance.close();
+
+      document.removeEventListener("keydown", event);
+    }
+  });
+}
